d1103dbf255b63046356fb36ea85a596d7a3b963
Adam Hennefer <adam.hennefer@gmail.com>
2020-06-30 22:26:48 +0000

    added icons in footers

--- new_user_login.php
+++ new_user_login.php
@@ -20,6 +20,7 @@
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="styles01.css">
 	<link rel="stylesheet" href="styles.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<style>
 		#error {
 		  color: red;	
@@ -92,9 +93,15 @@
 	</div>
 	</data>
 	<footer>
+		<table style="margin-left:auto;margin-right:auto;">
+			<tr>
+			<td><a href="https://github.com/alh-repo"><i class="fa fa-github"></i></a></td>
+			<td><a href="https://www.linkedin.com/in/adam-hennefer-59577116/"><i class="fa fa-linkedin-square"></i></a></td>			
+			<td><a href="mailto:ahennefer3@horizon.csueastbay.edu"><i class="fa fa-envelope"></i></a></td>	
+			</tr>
+		</table>	
 		<p>This website and the information found within was created for educational purposes only.</p>
 		<p>Authored by: Adam Hennefer</p>
-		<p>Contact information: <a href="mailto:ahennefer3@horizon.csueastbay.edu"> ahennefer3@horizon.csueastbay.edu</a></p>
 	</footer>
 </body>
 </html>
--- restricted.php
+++ restricted.php
@@ -6,8 +6,9 @@
 <!DOCTYPE html>
 
 <!--
-// Name: Adam Hennefer
-// Date: 12.9.19 
+// Author Name: Adam Hennefer
+// Date created: 12.9.19 
+// Last Updated: 6.30.20 
 // File: restricted.php
  -->
 <html>
@@ -19,9 +20,8 @@
 	<meta name="author" content="Adam Hennefer">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="styles.css">
-	<style>
-		
-	</style>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 </head>
 
 <body>
@@ -50,9 +50,15 @@
 		
 	</data>
    	<footer>
+		<table style="margin-left:auto;margin-right:auto;">
+			<tr>
+			<td><a href="https://github.com/alh-repo"><i class="fa fa-github"></i></a></td>
+			<td><a href="https://www.linkedin.com/in/adam-hennefer-59577116/"><i class="fa fa-linkedin-square"></i></a></td>			
+			<td><a href="mailto:ahennefer3@horizon.csueastbay.edu"><i class="fa fa-envelope"></i></a></td>	
+			</tr>
+		</table>	
 		<p>This website and the information found within was created for educational purposes only.</p>
 		<p>Authored by: Adam Hennefer</p>
-		<p>Contact information: <a href="mailto:ahennefer3@horizon.csueastbay.edu"> ahennefer3@horizon.csueastbay.edu</a></p>
 	</footer>
  <?php
 
